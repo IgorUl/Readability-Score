4c6c317cb8913216b0ce99ef9b143c4f28d05a9b
IgorUl <ulyakin.igor@gmail.com>
2020-05-31 22:17:45 +0300

    second stage

--- readability/Main.java
+++ readability/Main.java
@@ -6,7 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(str.length() > 100 ? "HARD" : "EASY");
+        String[] str;
+        int sum = 0;
+        int res;
+        str = sc.nextLine().split("[.!?]");
+        for ( String st: str) {
+            sum += st.trim().split("[\\p{Z}]").length;
+        }
+        if ((sum % str.length) != 0) {
+            res = sum / str.length + 1;
+        } else {
+            res = sum / str.length;
+        }
+        System.out.println(res > 10 ? "HARD" : "EASY");
     }
 }
