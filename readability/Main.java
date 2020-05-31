6c1fc64d741a504ac494727e3d3be6db21682bb7
IgorUl <ulyakin.igor@gmail.com>
2020-05-31 23:00:25 +0300

    work on third stage

--- readability/Main.java
+++ readability/Main.java
@@ -1,23 +1,31 @@
 package readability;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        String[] str;
-        int sum = 0;
-        int res;
-        str = sc.nextLine().split("[.!?]");
-        for ( String st: str) {
-            sum += st.trim().split("[\\p{Z}]").length;
+        try {
+            File file = new File(args[0]);
+            Scanner sc = new Scanner(file);
+            String text = sc.nextLine();
+            System.out.println();
+            System.out.println(text);
+//        int sum = 0;
+//        int res;
+//        str = sc.nextLine().split("[.!?]");
+//        for ( String st: str) {
+//            sum += st.trim().split("[\\p{Z}]").length;
+//        }
+//        if ((sum % str.length) != 0) {
+//            res = sum / str.length + 1;
+//        } else {
+//            res = sum / str.length;
+//        }
+//        System.out.println(res > 10 ? "HARD" : "EASY");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         }
-        if ((sum % str.length) != 0) {
-            res = sum / str.length + 1;
-        } else {
-            res = sum / str.length;
-        }
-        System.out.println(res > 10 ? "HARD" : "EASY");
     }
 }
